27801741346f9e49f41ead108f876c18953ec536
Yevgen <godun.yevgen@gmail.com>
2012-12-11 17:50:26 +0200

    footer, header, index.html(part1)

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-
+$(document).ready(function(){
+// cycle 
+ $('.js-scroll-index').cycle({ 
+    fx:     'fade',
+    timeout: 5000,
+    speed: 700
 });
+
+$(".more-var").next().hide();
+$(".more-var").click(function(){
+    $(this).slideUp("fast");
+    $(this).next().slideDown("fast");
+});
+
+})
