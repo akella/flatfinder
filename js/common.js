bd3de6ae5afb43914cbdbf95450100c10f417a4d
Yevgen <godun.yevgen@gmail.com>
2012-12-12 18:10:11 +0200

    index.html

--- js/common.js
+++ js/common.js
@@ -1,15 +1,29 @@
 $(document).ready(function(){
-// cycle 
- $('.js-scroll-index').cycle({ 
-    fx:     'fade',
-    timeout: 5000,
-    speed: 700
-});
 
-$(".more-var").next().hide();
-$(".more-var").click(function(){
-    $(this).slideUp("fast");
-    $(this).next().slideDown("fast");
-});
+    // cycle 
+     $('.js-scroll-index').cycle({ 
+        fx:     'fade',
+        timeout: 5000,
+        speed: 700
+    });
 
-})
+    // more
+    $(".more-var").next().hide();
+    $(".more-var").click(function(){
+        $(this).slideUp("fast");
+        $(this).next().slideDown("fast");
+    });
+
+    // show/hide popup enter
+    $(".popup").hide();
+    $(".popup-bg").hide();
+    $(".popup-bg").click(function(){
+        $(this).fadeOut();
+        $(".popup").fadeOut();
+    });
+    $(".enter").click(function(){
+        $(".popup_enter").fadeIn();
+        $(".popup-bg").fadeIn();
+    });
+
+});
