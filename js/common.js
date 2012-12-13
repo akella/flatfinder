8f514a9bf9e86a0f246fb1c126c714bfe2e610e9
Yevgen <godun.yevgen@gmail.com>
2012-12-13 12:35:11 +0200

    profile.html

--- js/common.js
+++ js/common.js
@@ -7,11 +7,33 @@ $(document).ready(function(){
         speed: 700
     });
 
+    $(".select-city strong").click(function(){
+        if ($(this).hasClass("active"))
+            {
+                $(this).next().hide();
+                $(this).removeClass("active");
+            }
+        else {
+                $(this).next().show();
+                $(this).addClass("active");
+             }
+        return false;
+    });
     // more
     $(".more-var").next().hide();
     $(".more-var").click(function(){
         $(this).slideUp("fast");
         $(this).next().slideDown("fast");
+    });
+
+    // select
+    $(".select span").each(function(){
+        $(this).text($(this).next().val());
+    });
+    
+    $(".select select").change(function(){
+        var val = $(this).val();
+        $(this).prev().text(val);
     });
 
     // show/hide popup enter
